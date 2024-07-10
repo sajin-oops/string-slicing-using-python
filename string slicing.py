3636555c2e6a51ba4c5d861287b591e2286f7e49
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-10 18:26:09 +0530

    Add files via upload

---
+++ string slicing.py
@@ -0,0 +1,10 @@
+name = "sajin is a good boy"
+print(name[0:5])
+print(name[-5:])
+print(name[:5])
+print(name[1:])
+a = "sajinhumbleguy"
+print(a[0:13:2])  #str[start:stop:step] format for three value
+print(a[-3:-1])
+print(a[-2:-6:-1])
+print(a[::3])
